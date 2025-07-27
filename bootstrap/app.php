85e8a120c8780ca2555f90bd85cbf4934ba8afb8
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-07-28 06:44:13 +0700

    fix: implement midtrans webhook and quest submission enhancements
    - Implemented Midtrans webhook handler to process transaction status updates.
    - Enhanced quest submission functionality to support file uploads and Google Drive links.
    - Updated route configurations to include API routes for Midtrans webhook.
    - Added CSRF token exception for Midtrans webhook URL to allow external requests.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(except: [
             'http://127.0.0.1:8000/api/midtrans/webhook',
+            'https://dapurtani.salmanabdurrahman.my.id/api/midtrans/webhook'
         ]);
 
         $middleware->alias([
